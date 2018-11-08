d123a9eb5256517ecf2a205161a0ee913dab7f5c
seror <seror94ab@yahoo.com>
2018-11-08 11:50:01 -0600

    imported particles js

--- my-portfolio/src/components/App.js
+++ my-portfolio/src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import "../App.css";
 import About from "./About";
 
+import Particles from "react-particles-js";
+
 class App extends Component {
   render() {
     return (
