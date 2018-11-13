7a5c93ddbb98656ad7a1d67fd95880104ac1a62c
seror <seror94ab@yahoo.com>
2018-11-12 20:47:39 -0600

    updated particles

--- my-portfolio/src/components/App.js
+++ my-portfolio/src/components/App.js
@@ -1,14 +1,46 @@
 import React, { Component } from "react";
 import "../App.css";
 import About from "./About";
-
 import Particles from "react-particles-js";
+
+const particleOpt = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    move: {
+      speed: 8
+    },
+    size: {
+      value: 2
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true
+      }
+    },
+    modes: {
+      push: {
+        particles_nb: 12
+      }
+    }
+  }
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <About />
+
+        <Particles params={particleOpt} />
       </div>
     );
   }
