eb5f26516ade1a6266e888b12295e1d4b678d1a4
seror <seror94ab@yahoo.com>
2018-11-19 15:40:16 -0600

    made links clickable

--- my-portfolio/src/components/App.js
+++ my-portfolio/src/components/App.js
@@ -17,6 +17,7 @@ class App extends Component {
             bottom: 0,
             right: 0,
             left: 0,
+            zIndex: -1,
           }}
         />
       </div>
--- my-portfolio/src/components/SideNav.js
+++ my-portfolio/src/components/SideNav.js
@@ -5,7 +5,7 @@ const routes = [
   {
     path: "/",
     exact: true,
-    main: () => <h2>Home</h2>,
+    main: () => <h2>Home!!!!!</h2>,
   },
   {
     path: "/About",
@@ -29,27 +29,31 @@ class SideNav extends React.Component {
   render() {
     return (
       <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/About">About</Link>
-            </li>
-            <li>
-              <Link to="/Skills">Skills</Link>
-            </li>
-            <li>
-              <Link to="/Projects">Projects</Link>
-            </li>
-            <li>
-              <Link to="Resume">Rsume</Link>
-            </li>
-          </ul>
-          {routes.map(route => (
-            <Route key={route.path} path={route.path} exact={route.exact} component={route.main} />
-          ))}
+        <div style={{ display: "flex" }}>
+          <div style={{ padding: "10px", width: "40%", background: "f0f0f0" }}>
+            <ul style={{ listStyleType: "none", padding: 0 }}>
+              <li>
+                <Link className="link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/About">About</Link>
+              </li>
+              <li>
+                <Link to="/Skills">Skills</Link>
+              </li>
+              <li>
+                <Link to="/Projects">Projects</Link>
+              </li>
+              <li>
+                <Link to="/Resume">Resume</Link>
+              </li>
+            </ul>
+            {routes.map(route => (
+              <Route key={route.path} path={route.path} exact={route.exact} component={route.main} />
+            ))}
+          </div>
         </div>
       </Router>
     );
