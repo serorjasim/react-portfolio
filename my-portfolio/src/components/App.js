3f94037f8dfa546f4a10e979d898d793af5c3c9a
seror <seror94ab@yahoo.com>
2018-11-20 16:27:55 -0600

    updated particles

--- my-portfolio/src/components/App.js
+++ my-portfolio/src/components/App.js
@@ -3,7 +3,6 @@ import { Route } from "react-router-dom";
 
 import { routes } from "../routes";
 
-import Particle from "./Particles";
 import SideBar from "./SideBar";
 
 class App extends Component {
@@ -12,16 +11,6 @@ class App extends Component {
       <div className="App" style={{ display: "flex" }}>
         <SideBar />
         <Content />
-        <Particle
-          style={{
-            position: "absolute",
-            top: 0,
-            bottom: 0,
-            right: 0,
-            left: 0,
-            zIndex: -1,
-          }}
-        />
       </div>
     );
   }
@@ -29,24 +18,7 @@ class App extends Component {
 
 const Content = props => {
   return (
-    <div
-      style={{
-        background: "white",
-
-        // margin: "auto",
-        borderRadius: "5px",
-        boxShadow: "5px 10px 10px #888888",
-
-        height: "300px",
-        width: "300px",
-
-        position: "fixed",
-        top: "50%",
-        left: "50%",
-        /* bring your own prefixes */
-        transform: "translate(-50%, -50%)",
-      }}
-    >
+    <div className="content">
       {routes.map(({ path, main, ...rest }) => (
         <Route path={path} component={main} {...rest} />
       ))}
--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-
 import Particle from "./components/Particles";
-
 import "./App.css";
 import "./index.css";
 import App from "./components/App";
@@ -13,6 +11,16 @@ ReactDOM.render(
     <Router>
       <App />
     </Router>
+    <Particle
+      style={{
+        position: "absolute",
+        top: 0,
+        bottom: 0,
+        right: 0,
+        left: 0,
+        zIndex: 0,
+      }}
+    />
   </React.Fragment>,
   document.getElementById("root")
 );
