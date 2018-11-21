3a6c62bb5b0e62f6de7370fa41eca382ae9e283a
seror <seror94ab@yahoo.com>
2018-11-21 11:17:45 -0600

    added icons to the home page

--- my-portfolio/src/components/Home.js
+++ my-portfolio/src/components/Home.js
@@ -1,13 +1,24 @@
 import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Home = () => (
   <div className="home-container">
     <h1 className="title">Seror Jasim</h1>
     <h3 className="work">Software Engineer</h3>
     <div className="icons">
-      <a href="https://github.com/serorjasim">github</a>
-      <a href="https://www.linkedin.com/in/seror-jasim/">linkedIn</a>
-      <a href="mailto:serorajasim@gmail.com">email</a>
+      <a href="https://github.com/serorjasim">
+        <FontAwesomeIcon icon={faGithub} />
+      </a>
+
+      <a href="https://www.linkedin.com/in/seror-jasim/">
+        <FontAwesomeIcon icon={faLinkedin} />
+      </a>
+      <a href="mailto:serorajasim@gmail.com">
+        <FontAwesomeIcon icon={faEnvelope} />
+      </a>
     </div>
   </div>
 );
