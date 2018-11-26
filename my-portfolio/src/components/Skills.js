1edb9d1ac19262fb581ed28a44186c371f2797bc
seror <seror94ab@yahoo.com>
2018-11-26 12:18:42 -0600

    added skills

--- my-portfolio/src/components/Skills.js
+++ my-portfolio/src/components/Skills.js
@@ -3,8 +3,42 @@ import React from "react";
 const Skills = () => (
   <div className="skills-container">
     <h1 className="skills-title">Skills</h1>
-    <i class="devicon-react-original-wordmark colored" />
-    <i class="devicon-javascript-plain colored" />
+    <span className="skill">
+      <i class="devicon-javascript-plain colored" />
+      <p>Javascript</p>
+    </span>
+    <span className="skill">
+      <i class="devicon-react-original colored" />
+      <p>React</p>
+    </span>
+    <span className="skill">
+      <i class="devicon-nodejs-plain colored" />
+      <p>Node</p>
+    </span>
+    <span className="skill">
+      <i class="devicon-jquery-plain colored" />
+      <p>jQuery</p>
+    </span>
+    <span className="skill">
+      <i class="devicon-mysql-plain colored" />
+      <p>MySQL</p>
+    </span>
+    <span className="skill">
+      <i class="devicon-mongodb-plain colored" />
+      <p>MongoDB</p>
+    </span>
+    <span className="skill">
+      <i class="devicon-express-original colored" />
+      <p>Express</p>
+    </span>
+    <span className="skill">
+      <i class="devicon-html5-plain colored" />
+      <p>HTML</p>
+    </span>
+    <span className="skill">
+      <i class="devicon-css3-plain colored" />
+      <p>CSS</p>
+    </span>
   </div>
 );
 
