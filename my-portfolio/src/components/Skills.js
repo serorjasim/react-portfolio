f39eb01021341edcb4939911a8670c8385efa403
seror <seror94ab@yahoo.com>
2018-11-28 17:56:27 -0600

    skills style update

--- my-portfolio/src/components/Skills.js
+++ my-portfolio/src/components/Skills.js
@@ -4,39 +4,39 @@ const Skills = () => (
   <div className="skills-container">
     <h1 className="skills-title">Skills</h1>
     <span className="skill">
-      <i class="devicon-javascript-plain colored" />
+      <i className="devicon-javascript-plain colored" />
       <p>Javascript</p>
     </span>
     <span className="skill">
-      <i class="devicon-react-original colored" />
+      <i className="devicon-react-original colored" />
       <p>React</p>
     </span>
     <span className="skill">
-      <i class="devicon-nodejs-plain colored" />
+      <i className="devicon-nodejs-plain colored" />
       <p>Node</p>
     </span>
     <span className="skill">
-      <i class="devicon-jquery-plain colored" />
+      <i className="devicon-jquery-plain colored" />
       <p>jQuery</p>
     </span>
     <span className="skill">
-      <i class="devicon-mysql-plain colored" />
+      <i className="devicon-mysql-plain colored" />
       <p>MySQL</p>
     </span>
     <span className="skill">
-      <i class="devicon-mongodb-plain colored" />
+      <i className="devicon-mongodb-plain colored" />
       <p>MongoDB</p>
     </span>
     <span className="skill">
-      <i class="devicon-express-original colored" />
+      <i className="devicon-express-original colored" />
       <p>Express</p>
     </span>
     <span className="skill">
-      <i class="devicon-html5-plain colored" />
+      <i className="devicon-html5-plain colored" />
       <p>HTML</p>
     </span>
     <span className="skill">
-      <i class="devicon-css3-plain colored" />
+      <i className="devicon-css3-plain colored" />
       <p>CSS</p>
     </span>
   </div>
