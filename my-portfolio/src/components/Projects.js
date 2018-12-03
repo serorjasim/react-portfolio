53307bbfb4169aa3954347fe2299d829b8d3514a
seror <seror94ab@yahoo.com>
2018-12-03 14:30:40 -0600

    imported images into projects component

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -1,8 +1,12 @@
 import React from "react";
+import EDC from "../images/EDC.png";
+import train from "../images/train.png";
 
 const Projects = () => (
   <div className="projects-container">
     <h1 className="projects-title">Projects</h1>
+    <img className="projects-images" src={EDC} alt={"Eat-Drink-Cook"} />
+    <img className="projects-images" src={train} alt={"train"} />
   </div>
 );
 
