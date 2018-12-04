8aaa71ee716138941b83ecd91e0c5a3bf5d721f7
seror <seror94ab@yahoo.com>
2018-12-04 13:05:09 -0600

    added styling to projects

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -5,19 +5,17 @@ import train from "../images/train.png";
 const Projects = () => (
   <div className="projects-container">
     <h1 className="projects-title">Projects</h1>
-    <ul>
-      <li>
-        <a href="https://serorjasim.github.io/TeamProject1/" target="_blank">
-          <img className="projects-images" src={EDC} alt={"Eat-Drink-Cook"} />
-          EDC
-        </a>
-      </li>
-      <li>
-        <a href="https://serorjasim.github.io/Train-Scheduler/" target="_blank">
-          <img className="projects-images" src={train} alt={"train"} />
-        </a>
-      </li>
-    </ul>
+    <div className="projects-group">
+      <a className="anchor" href="https://serorjasim.github.io/TeamProject1/" target="_blank">
+        <img className="projects-images" src={EDC} alt={"Eat-Drink-Cook"} />
+        <h3 className="project-name">EDC</h3>
+      </a>
+
+      <a className="anchor" href="https://serorjasim.github.io/Train-Scheduler/" target="_blank">
+        <img className="projects-images" src={train} alt={"train"} />
+        <h3 className="project-name">Train Scheduler</h3>
+      </a>
+    </div>
   </div>
 );
 
