effddb0a9b3b895a97f69acd97afe07a54b9afde
seror <seror94ab@yahoo.com>
2018-12-03 18:05:03 -0600

    updated projects page

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -5,8 +5,19 @@ import train from "../images/train.png";
 const Projects = () => (
   <div className="projects-container">
     <h1 className="projects-title">Projects</h1>
-    <img className="projects-images" src={EDC} alt={"Eat-Drink-Cook"} />
-    <img className="projects-images" src={train} alt={"train"} />
+    <ul>
+      <li>
+        <a href="https://serorjasim.github.io/TeamProject1/" target="_blank">
+          <img className="projects-images" src={EDC} alt={"Eat-Drink-Cook"} />
+          EDC
+        </a>
+      </li>
+      <li>
+        <a href="https://serorjasim.github.io/Train-Scheduler/" target="_blank">
+          <img className="projects-images" src={train} alt={"train"} />
+        </a>
+      </li>
+    </ul>
   </div>
 );
 
