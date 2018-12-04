dcb9ec9b19d3ba461ad9c502f033da92876d3275
seror <seror94ab@yahoo.com>
2018-12-04 13:15:13 -0600

    update

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -9,11 +9,17 @@ const Projects = () => (
       <a className="anchor" href="https://serorjasim.github.io/TeamProject1/" target="_blank">
         <img className="projects-images" src={EDC} alt={"Eat-Drink-Cook"} />
         <h3 className="project-name">EDC</h3>
+        <a className="anchor" href="https://github.com/serorjasim/TeamProject1" target="_blank">
+          <h3 className="repo">Github</h3>
+        </a>
       </a>
 
       <a className="anchor" href="https://serorjasim.github.io/Train-Scheduler/" target="_blank">
         <img className="projects-images" src={train} alt={"train"} />
         <h3 className="project-name">Train Scheduler</h3>
+        <a className="anchor" href="https://github.com/serorjasim/Train-Scheduler" target="_blank">
+          <h3 className="repo">Github</h3>
+        </a>
       </a>
     </div>
   </div>
