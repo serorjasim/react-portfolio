9896891197f41f6ef16f55082c21a0db7eda090c
seror <seror94ab@yahoo.com>
2018-12-05 16:07:34 -0600

    added styling

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -4,7 +4,13 @@ const About = () => (
   <div className="about-container">
     <h1 className="about-title">About Me</h1>
     <br />
-    <p>
+    <p className="about-description">
+      is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+      text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
+      book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining
+      essentially unchanged.
+    </p>
+    <p className="about-description">
       is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
       text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
       book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining
