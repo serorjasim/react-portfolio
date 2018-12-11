d13ec7ac61d98f4dfa11aabc9d410edc20af9b46
seror <seror94ab@yahoo.com>
2018-12-11 17:38:15 -0600

    updated skills style

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -10,6 +10,7 @@ const About = () => (
       book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining
       essentially unchanged.
     </p>
+    <br />
     <p className="about-description">
       is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
       text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
