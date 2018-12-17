aea88a652d2579c39983714793146ce7fcf0d232
seror <seror94ab@yahoo.com>
2018-12-17 17:54:15 -0600

    added transtion to skills container

--- my-portfolio/src/components/Skills.js
+++ my-portfolio/src/components/Skills.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 
 const SkillsGroup = ({ skills }) => {
   return (
@@ -16,32 +17,40 @@ const SkillsGroup = ({ skills }) => {
 };
 
 const Skills = () => (
-  <div className="skills-container">
-    <h1 className="skills-title">Skills</h1>
-    <SkillsGroup
-      skills={[
-        { name: "Javascript", icon: "devicon-javascript-plain" },
-        { name: "React", icon: "devicon-react-original" },
-        { name: "Node", icon: "devicon-nodejs-plain" },
-      ]}
-    />
+  <ReactCSSTransitionGroup
+    transitionName="fade"
+    transitionAppear={true}
+    transitionEnterTimeout={1000}
+    transitionLeaveTimeout={1000}
+    transitionAppearTimeout={1000}
+  >
+    <div className="skills-container">
+      <h1 className="skills-title">Skills</h1>
+      <SkillsGroup
+        skills={[
+          { name: "Javascript", icon: "devicon-javascript-plain" },
+          { name: "React", icon: "devicon-react-original" },
+          { name: "Node", icon: "devicon-nodejs-plain" },
+        ]}
+      />
 
-    <SkillsGroup
-      skills={[
-        { name: "jQuery", icon: "devicon-jquery-plain" },
-        { name: "MySQL", icon: "devicon-mysql-plain" },
-        { name: "MongoDB", icon: "devicon-mongodb-plain" },
-      ]}
-    />
+      <SkillsGroup
+        skills={[
+          { name: "jQuery", icon: "devicon-jquery-plain" },
+          { name: "MySQL", icon: "devicon-mysql-plain" },
+          { name: "MongoDB", icon: "devicon-mongodb-plain" },
+        ]}
+      />
 
-    <SkillsGroup
-      skills={[
-        { name: "Express", icon: "devicon-express-original" },
-        { name: "HTML", icon: "devicon-html5-plain" },
-        { name: "CSS", icon: "devicon-css3-plain" },
-      ]}
-    />
-  </div>
+      <SkillsGroup
+        skills={[
+          { name: "Express", icon: "devicon-express-original" },
+          { name: "HTML", icon: "devicon-html5-plain" },
+          { name: "CSS", icon: "devicon-css3-plain" },
+        ]}
+      />
+    </div>
+  </ReactCSSTransitionGroup>
 );
 
 export default Skills;
