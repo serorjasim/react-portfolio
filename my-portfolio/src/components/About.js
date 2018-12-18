cf03ec6d9180613b5bfa3ba36d9722c14db2a211
seror <seror94ab@yahoo.com>
2018-12-18 17:49:32 -0600

    added more details to about me page

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -15,13 +15,19 @@ const About = () => (
       <br />
       <img className="about-image" src={seror} alt={"seror"} />
       <p className="about-description">
-        I’m software engineer based in Austin, TX. I enjoy playing soccer, live music, and learning new things.
+        I’m a software engineer based in Austin, TX. I enjoy playing soccer, live music, and learning new things.
       </p>
       <br />
       <p className="about-description">
         After moving to Austin few years ago, I met people who did computer programming. As I learned about it, I knew
         right away that’s what I wanted to do. I took a coding bootcamp at the University of Texas. There I learned the
         fundemantals of programming in different languages and frameworks.
+      </p>
+      <br />
+      <p className="about-description">
+        I have worked as an Interpreter. While doing that, I learned that attention to detail is key to being
+        successful. I’m always eager to improve my skills and learn new technologies. Therefor I believe I can
+        contribute to the success of any team when given the chance.
       </p>
     </div>
   </ReactCSSTransitionGroup>
