e02c78f5d5137827a792f6e1baa7c85134b6ce31
seror <seror94ab@yahoo.com>
2018-12-17 18:35:49 -0600

    added transition to projects page

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -2,36 +2,45 @@ import React from "react";
 import EDC from "../images/EDC.png";
 import train from "../images/train.png";
 import giphy from "../images/giphy.png";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 
 const Projects = () => (
-  <div className="projects-container">
-    <h1 className="projects-title">Projects</h1>
-    <div className="projects-group">
-      <a className="anchor" href="https://serorjasim.github.io/TeamProject1/" target="_blank">
-        <img className="projects-images" src={EDC} alt={"Eat-Drink-Cook"} />
-        <h3 className="project-name">EDC</h3>
-        <a className="anchor" href="https://github.com/serorjasim/TeamProject1" target="_blank">
-          <h3 className="repo">Github</h3>
+  <ReactCSSTransitionGroup
+    transitionName="fade"
+    transitionAppear={true}
+    transitionEnterTimeout={1000}
+    transitionLeaveTimeout={1000}
+    transitionAppearTimeout={1000}
+  >
+    <div className="projects-container">
+      <h1 className="projects-title">Projects</h1>
+      <div className="projects-group">
+        <a className="anchor" href="https://serorjasim.github.io/TeamProject1/" target="_blank">
+          <img className="projects-images" src={EDC} alt={"Eat-Drink-Cook"} />
+          <h3 className="project-name">EDC</h3>
+          <a className="anchor" href="https://github.com/serorjasim/TeamProject1" target="_blank">
+            <h3 className="repo">Github</h3>
+          </a>
         </a>
-      </a>
 
-      <a className="anchor" href="https://serorjasim.github.io/Train-Scheduler/" target="_blank">
-        <img className="projects-images" src={train} alt={"train"} />
-        <h3 className="project-name">Train Scheduler</h3>
-        <a className="anchor" href="https://github.com/serorjasim/Train-Scheduler" target="_blank">
-          <h3 className="repo">Github</h3>
+        <a className="anchor" href="https://serorjasim.github.io/Train-Scheduler/" target="_blank">
+          <img className="projects-images" src={train} alt={"train"} />
+          <h3 className="project-name">Train Scheduler</h3>
+          <a className="anchor" href="https://github.com/serorjasim/Train-Scheduler" target="_blank">
+            <h3 className="repo">Github</h3>
+          </a>
         </a>
-      </a>
 
-      <a className="anchor" href="https://serorjasim.github.io/GIPHY/" target="_blank">
-        <img className="projects-images" src={giphy} alt={"giphy"} />
-        <h3 className="project-name">Giphy</h3>
-        <a className="anchor" href="https://github.com/serorjasim/GIPHY" target="_blank">
-          <h3 className="repo">Github</h3>
+        <a className="anchor" href="https://serorjasim.github.io/GIPHY/" target="_blank">
+          <img className="projects-images" src={giphy} alt={"giphy"} />
+          <h3 className="project-name">Giphy</h3>
+          <a className="anchor" href="https://github.com/serorjasim/GIPHY" target="_blank">
+            <h3 className="repo">Github</h3>
+          </a>
         </a>
-      </a>
+      </div>
     </div>
-  </div>
+  </ReactCSSTransitionGroup>
 );
 
 export default Projects;
