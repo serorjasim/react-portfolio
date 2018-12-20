2f6246b2d76ca9a8702dcb16019405b760aeda45
seror <seror94ab@yahoo.com>
2018-12-20 14:42:27 -0600

    added image to about me

--- my-portfolio/src/components/Seror.js
+++ my-portfolio/src/components/Seror.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Home from "./Home";
 
 const Seror = () => (
-  <div>
-    <p className="my-name">seror</p>
+  <div className="seror-container">
+    <p className="my-name">
+      <a href="https://www.w3schools.com">Visit W3Schools.com!</a>
+    </p>
   </div>
 );
 
